aa44c17e0d595742aff17964423e8ce0fccc9806
Bisma421 <bismagul421@gmail.com>
2024-05-16 12:50:39 +0500

    TODO-List Application

---
+++ main.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList = [];
+let condition = "true";
+console.log(chalk.magentaBright.bold("\n\tTODO-LIST\n"));
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.blackBright("Enter your task:")
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task} Task added sucessfully`);
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addMore",
+            type: "confirm",
+            message: chalk.blackBright("Do you want to add more task?"),
+            default: false
+        }
+    ]);
+    condition = addMoreTask.addMore;
+}
+console.log("Your updated todo-list:", todoList);
